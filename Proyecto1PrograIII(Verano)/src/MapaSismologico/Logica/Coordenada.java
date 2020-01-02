4f4c152c5ccffec1a52ac0e3f415781013255d57
PamCS97 <p.collado20@gmail.com>
2020-01-01 20:07:07 -0600

    Archivos nuevos
    Se agregaron 2 archvos xml y una clase "Coordenada"

---
+++ Proyecto1PrograIII(Verano)/src/MapaSismologico/Logica/Coordenada.java
@@ -0,0 +1,30 @@
+package MapaSismologico.Logica;
+
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ *
+ * @author Pamela
+ */
+public class Coordenada {
+    
+    public Coordenada(){
+        
+    }
+    
+public void leerXML(String archivo){
+     try {
+            JAXBContext ctx = JAXBContext.newInstance(Coordenada.class);
+            Unmarshaller mrs = ctx.createUnmarshaller();
+            Object obj = mrs.unmarshal(new File(archivo));
+            System.out.println(obj);
+
+        } catch (JAXBException ex) {
+            System.err.printf("Excepción: '%s'%n", ex.getMessage());
+        }
+}
+    
+}
