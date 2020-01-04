f2e0306937971d6cf530d81ab74eca1203921692
PamCS97 <p.collado20@gmail.com>
2020-01-04 13:38:52 -0600

    Nueva clase posicion de la imagen
    Almacena la posicion 'x' y 'y'

---
+++ Proyecto1PrograIII(Verano)/src/MapaSismologico/Logica/ImagePosition.java
@@ -0,0 +1,18 @@
+package MapaSismologico.Logica;
+
+/**
+ *
+ * @author Pamela
+ */
+public class ImagePosition {
+
+    public ImagePosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    
+    private int x;
+    private int y;
+    
+}
