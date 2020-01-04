c70cfcd285a1e0d6379d639cea49e8ddae043d53
PamCS97 <p.collado20@gmail.com>
2020-01-04 13:37:56 -0600

    Nueva clase Imagen
    Almacena el url y la dimension de la imagen

---
+++ Proyecto1PrograIII(Verano)/src/MapaSismologico/Logica/Image.java
@@ -0,0 +1,18 @@
+package MapaSismologico.Logica;
+
+/**
+ *
+ * @author Pamela
+ */
+public class Image {
+
+    public Image(String url, Dimension dimen) {
+        this.url = url;
+        this.dimen = dimen;
+    }
+
+    
+    private String url;
+    private Dimension dimen;
+    
+}
