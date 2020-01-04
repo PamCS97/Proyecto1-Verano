b81a79064c59b665351c88940cc5629d59204300
PamCS97 <p.collado20@gmail.com>
2020-01-04 13:35:26 -0600

    Nueva clase Dimension
    clase que se utiliza para almacenar el ancho y largo de la imagen

---
+++ Proyecto1PrograIII(Verano)/src/MapaSismologico/Logica/Dimension.java
@@ -0,0 +1,16 @@
+package MapaSismologico.Logica;
+
+/**
+ *
+ * @author Pamela
+ */
+public class Dimension {
+
+    public void Dimension(int weight, int height){
+        this.height = height;
+        this.weigth = weigth;
+    }
+    
+private int weigth;
+private int height;
+}
